f60c4727e9ea4183827b23a64d49c93209555558
Claire Tuna <clairetuna@unknown3c15c2eb1ff2.att.net>
2015-07-27 20:07:16 -0700

    converted hardcored to scale for yAxis

--- javascripts/vis.js
+++ javascripts/vis.js
@@ -4,6 +4,7 @@
 var svg; 
 var w = 500;
 var h = 400; 
+var r; 
 
 // data
 var totalRecVisitors = 0;
@@ -18,7 +19,7 @@ init = function( s ){
 
 // load csv data into var dataset
 loadData = function(){ 
-	console.log( "loading data...");
+	console.log( "loading data..." );
 	d3.csv( "../data/yosemite2014.csv", function( error, d ){
 		if( error ){
 			console.log( error );
@@ -42,38 +43,43 @@ editData = function(){
 		}
 	} )
 	console.log( "total recreational visitors: " + totalRecVisitors );
-	console.log( "max recreational visitors/month : " + maxVisitors);
+	console.log( "max recreational visitors/month : " + maxVisitors );
 	generateVis();
 }
 
 
 generateVis = function(){
-	console.log( "generating visualization...")
+	console.log( "generating visualization..." )
+
+	r = w / dataset.length / 2 * .40;
+
+	var yScale = d3.scale.linear()
+			.domain([0, maxVisitors])
+			.range([0, h - r]);
+
 	svg.attr("width", w)
 		.attr("height", h);
-	var circles = svg.selectAll("circle")
-		.data(dataset)
+
+	var circles = svg.selectAll( "circle" )
+		.data( dataset )
 		.enter()
-		.append("circle")
-		.attr("fill", "green");
-
-	circles.attr("cx", function(d, i) {
-		return (i * 50) + 25;
-	})
-		.attr("cy", function (d){
-
-			//console.log( d.Field3/ totalRecVisitors * 100 );
-			return h - d.Field3 / ( maxVisitors) * ( h - 20 );
+		.append( "circle" )
+		.attr( "fill", "green" )
+		.attr( "cx", function( d, i ) {
+			return ( i * 50 ) + 25;
 		})
-		.attr("r", 8 )
-		.on('mouseover', function(d){
-			var coordinates = [0, 0];
-			coordinates = d3.mouse(this);
-			var x = coordinates[0];
-			var y = coordinates[1];	
+		.attr( "cy", function ( d ){
+			return h - yScale( d.Field3 );
+		})
+		.attr( "r", r )
+		.on('mouseover', function( d ){
+			var coordinates = [ 0, 0 ];
+			coordinates = d3.mouse( this );
+			var x = coordinates[ 0 ];
+			var y = coordinates[ 1 ];	
 			mouseover( d, x, y ); 
 		})
-		.on('mouseout', function(d){
+		.on('mouseout', function( d ){
 			mouseoff( d );
 		})
 	
@@ -81,40 +87,40 @@ generateVis = function(){
 
 // clear the visualization
 clear = function(){
-	svg.selectAll("circle").remove();
+	svg.selectAll( "circle" ).remove();
 }
 
 // redraw the visualization 
 redraw = function(){
-	console.log("redrawing vis");
+	console.log( "redrawing vis" );
 	clear();
 	generateVis();
 }
 
 mouseover = function( d, x, y ){
-	console.log( d.Field3 + " [ "+ x + ", " + y + "]");
-	svg.selectAll("rect")
-	.data( { x: x, y: y, d: d.Field3 })
-	.enter()
-	.append("rect")
-	.attr({
-		x: function( d ) {d.x},  
-		y: function( d ) {d.y}, 
-		width: 60,
-		height: 20,
-		fill: "red"
+	console.log( d.Field3 + " [ "+ x + ", " + y + "]" );
+	svg.selectAll( "rect" )
+		.data( { x: x, y: y, d: d.Field3 } )
+		.enter()
+		.append( "rect" )
+		.attr({
+			x: function( d ) { d.x },  
+			y: function( d ) { d.y }, 
+			width: 60,
+			height: 20,
+			fill: "red"
 		});
 	}
 
-mouseoff = function(d){
+mouseoff = function( d ){
 	redraw();
 }
 
 removeCommas = function( num ){
-	var i = num.indexOf(",");
+	var i = num.indexOf( "," );
 	while( i > -1 ){ //error
-		num = num.substr( 0, i ).concat( num.substr( i+1, num.length ))
-		var i = num.indexOf(",");
+		num = num.substr( 0, i ).concat( num.substr( i+1, num.length ) )
+		var i = num.indexOf( "," );
 	}
 	return num;
 }
